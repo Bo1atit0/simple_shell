b65a84aa720298480320e97b334d6fe44272bc18
jennycode7 <jennessecosmas7@gmail.com>
2023-10-18 06:17:15 -0700

    SimpleShell"

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @ac: argument count
+ * @av: argument
+ * @env: environment
+ * Return: int
+ */
+int main(int ac, char **av, char **env)
+{
+	hsh(ac, av, env);
+
+	return (EXIT_SUCCESS);
+}
