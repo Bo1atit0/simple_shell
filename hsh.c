dcf020a2f3344e1cbd27a56803ef410874668b3b
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-20 18:12:39 -0700

    hsh

--- hsh.c
+++ hsh.c
@@ -1,124 +1,79 @@
 #include "main.h"
-
+void str_len(void);
 /**
 *hsh - main entry point of the shell program
 *@ac: The number of command-line arguments.
 *@argv: An array of command-line argument strings.
 *@env: An array of environment variable strings.
 *
-*Description: It takes input from the user, parses it,
-*and executes the corresponding command *or program.
+*Description: It takes input from the user
+*, parses it, and executes the
+*corresponding command *or program.
 *
-*Return: 0 on successful execution, or EXIT_FAILURE on failure.
+*Return: 0 on successful execution,
+*or EXIT_FAILURE on failure.
 */
-
 int hsh(int ac, char **argv, char **env)
 {
-	structo container[] = {{NULL}, {NULL}, {NULL}, {0}, {0}};
-	int x, line, parse_ret, executor_ret = 0;
-
-	(void)argv;
-	while (1)
-	{
-		signal(SIGINT, handle_sig);
-		if (interactive_or_not())
-			printf("$ ");
-		line = _getline(container);
-		if (line == -1)
-		{
-			if (interactive_or_not())
-				putchar('\n');
-			return (EXIT_FAILURE);
-		}
-		if (*container->input == '\n' || *container->input == '\0'
-				|| *container->input == '#')
-		{
-			if (container->input != NULL)
-				free(container->input);
-			continue;
-		}
-		parse_ret = parse_line(container);
-		if (parse_ret == -1)
-		{
-			if (!interactive_or_not())
-				printf("\n");
-			free(container->input);
-			continue;
-		}
-		for (x = 0; container->parsed[x]; x++)
-			;
-		container->args = x;
-		executor_ret = executor(ac, argv, container, env), release(container);
-		if (executor_ret == 2)
-			if (!interactive_or_not())
-				exit(container->exit);
-	}
-	return (0);
-}
-
-/**
-*env_opt - prints the environment variables
-*@env: environment variables
-*@str: string to print
-*
-*Description: a function that prints the environment variables
-*
-*Return: env
-*/
-
-char *env_opt(char **env, char *str)
+structo container[] = {{NULL}, {NULL}, {NULL}, {0}, {0}};
+int x, line, parse_ret, executor_ret = 0;
+(void)argv;
+if (ac == 2)
 {
-	int x;
-
-	for (x = 0; env[x]; x++)
-	{
-		if (commence(env[x], str) == 0)
-		{
-			return (env[x]);
-		}
-	}
-	return (NULL);
+/** set_readfd(structure, argv); **/
 }
-
-/**
-*commence - compares two strings
-*@str: pointer to first string
-*@ptr: pointer to second string
-*
-*Description: compares the characters in two *strings,
-*`str` and *`ptr`, starting from the beginning and continuing until a
-*difference is found or the *end of either string is reached.
-*
-*Return: 0 or 1
-*/
-
-int commence(char *str, char *ptr)
+while (1)
 {
-	int i = 0;
-
-	while (ptr[i])
-	{
-		if (str[i] != ptr[i])
-		{
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-/**
- * release - frees memory
- * @container: struct
- */
-void release(structo *container)
+signal(SIGINT, handle_sig);
+if (interactive_or_not())
 {
-	int x;
-
-	for (x = 0; container->parsed[x]; x++)
-	{
-		free(container->parsed[x]);
-	}
-	free(container->parsed);
-	free(container->input);
+printf("$ ");
 }
+line = _getline(container);
+if (line == -1)
+{
+if (interactive_or_not())
+{
+putchar('\n');
+}
+return (EXIT_FAILURE);
+/* handle error */
+}
+if (*container->input == '\n' || *container->input == '\0'
+	|| *container->input == '#')
+{
+if (container->input != NULL)
+{
+free(container->input);
+}
+continue;
+}
+parse_ret = parse_line(container);
+if (parse_ret == -1)
+{
+if (!interactive_or_not())
+/*printf("\n");*/  /*removed for task 1 to check all */
+free(container->input);
+continue;
+}
+if (container->parsed == NULL)
+{
+printf("\n");
+continue;
+}
+for (x = 0; container->parsed[x]; x++)
+{
+;
+}
+container->args = x;
+executor_ret = executor(ac, argv, container, env);
+release(container);
+if (executor_ret == 2)
+{
+if (!interactive_or_not())
+exit(container->exit);
+}
+}
+return (0);
+}
+
