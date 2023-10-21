6f01fdbdd59a8ef0c7635e358b62a675966f97b0
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-20 18:13:27 -0700

    mainc

--- main.c
+++ main.c
@@ -13,3 +13,24 @@ int main(int ac, char **av, char **env)
 
 	return (EXIT_SUCCESS);
 }
+
+/**
+*handle_sig - handle_sig
+*@it: integer representing the received signal (should be SIGINT).
+*
+*Description: Signal handler for the SIGINT signal (Ctrl+C).
+*/
+
+void handle_sig(int it)
+{
+int x = 0;
+
+if (it == SIGINT)
+{
+x = write(STDOUT_FILENO, "\n$ ", 3);
+if (x == -1)
+{
+;
+}
+}
+}
