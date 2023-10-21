d9d89eff5e68d3e2ab2357c788ae2890af3b0cb2
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-20 18:11:45 -0700

    get input

--- get_input.c
+++ get_input.c
@@ -1,77 +1,68 @@
 #include "main.h"
 
 /**
- * _getline - get input from user.
- * @container: struct pointer.
- * Return: returns int.
- */
+*_getline - reads input from file stream
+*
+*@container: buffer for storing string
+*
+*Desc: reads input from stdin into a buffer
+*of size size and dynamically allocates memory
+*
+*Return: number of bytes read
+*/
+
 int _getline(structo *container)
 {
-	ssize_t lineptr;
-	size_t x;
-	int y;
-	char *strint = NULL;
+ssize_t lineptr;
+size_t x;
+int y;
+char *strint = NULL;
 
 
-	lineptr = getline(&strint, &x, stdin);
-	if (lineptr == -1)
-	{
-		if (strint != NULL)
-		{
-			free(strint);
-		}
-		return (-1);
-	}
+lineptr = getline(&strint, &x, stdin);
 
-	for (y = 0; strint[y]; y++)
-	{
-		if (strint[y] == '#' && y != 0)
-		{
-			y--;
-			if (strint[y] == ' ')
-			{
-				y++;
-				strint[y] = '\0';
-			}
-			else
-			{
-				break;
-			}
-		}
-	}
+if (lineptr == -1)
+{
+if (strint != NULL)
+{
+free(strint);
+}
+return (-1);
+}
 
-	container->input = strdup(strint);
+for (y = 0; strint[y]; y++)
+{
+if (strint[y] == '#' && y != 0)
+{
+y--;
+if (strint[y] == ' ')
+{
+y++;
+strint[y] = '\0';
+break;
+}
+else
+{
+y++;
+}
+}
+}
 
-	free(strint);
-	return (1);
+container->input = strdup(strint);
+
+free(strint);
+return (1);
 }
 
 /**
- * interactive_or_not - from stdin or not.
- * Return: int.
- */
-
+*interactive_or_not - if shell is in interactive mode or not
+*
+*Description: Checks if the program is running in an interactive terminal.
+*
+*Return: 1 if in interactive mode 0 if not
+*
+*/
 int interactive_or_not(void)
 {
-	return (isatty(STDIN_FILENO));
-}
-
-
-/**
- * handle_sig - handle signals.
- * @it: int.
- */
-
-void handle_sig(int it)
-{
-	int x = 0;
-
-	if (it == SIGINT)
-	{
-		x = write(STDOUT_FILENO, "\n$ ", 3);
-		if (x == -1)
-		{
-			;
-		}
-	}
+return (isatty(STDIN_FILENO));
 }
